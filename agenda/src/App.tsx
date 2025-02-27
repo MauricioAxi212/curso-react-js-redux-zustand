4831359b741bf49028fc094fcba87005cabb7595
MauricioAxi212 <mauricio.sanchezn@axity.com>
2025-02-27 10:11:46 -0600

    initial commit :tada:

---
+++ agenda/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import './App.css'
+import Login from './pages/Login'
+import Home from './pages/home'
+function App() {
+  return (
+   <Router>
+    <Routes>
+      <Route path='/login' element={<Login/>} />
+      <Route path='/home' element={<Home/>} />
+    </Routes>
+   </Router>
+  )
+}
+
+export default App
